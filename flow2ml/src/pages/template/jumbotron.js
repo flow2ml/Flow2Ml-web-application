acfe124683dca3492ca20411c32ac0b0a2f3faa3
yvkrishna <vedhakrishnapanchami@gmail.com>
2021-01-05 13:30:54 +0530

    Added Documentation

--- flow2ml/src/pages/template/jumbotron.js
+++ flow2ml/src/pages/template/jumbotron.js
@@ -9,7 +9,7 @@ export default function Jumbotron() {
                 <h1 className="display-4" className={styles.jumboHeadding}>Write only a Few Lines of<br /> Machine learning code using <br /><i><span className={styles.orgNameBig}>Flow2ML</span></i></h1>
                 <hr className="my-4" />
                 <p className="lead">Quickly design and customize pre-processing workflow in machine learning.<br />
-                Obtain taining, validating samples with only <span className={styles.highlightNumber}>3&nbsp;</span> 
+                Obtain training, validating samples with only <span className={styles.highlightNumber}>3&nbsp;</span> 
                 lines of code using <span className={styles.orgName}>Flow2ML</span> toolkit<br />
                 Check Installation and sample code to flow into your ml model fastly.
                 </p>
